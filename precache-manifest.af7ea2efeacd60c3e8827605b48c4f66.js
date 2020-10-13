1ff9e92c53c9e0266643f5650c050c2709c5084f
ec18270 <m.krisztian@se18.qmul.ac.uk>
2020-10-13 20:28:49 +0100

    Updates

---
+++ precache-manifest.af7ea2efeacd60c3e8827605b48c4f66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d36c08514fd87e43a42dbc9e0a45139",
+    "url": "/OkosCucc_kart/index.html"
+  },
+  {
+    "revision": "843d4029bbd4d0b10570",
+    "url": "/OkosCucc_kart/static/css/main.b01e181a.chunk.css"
+  },
+  {
+    "revision": "7b094961525603f90082",
+    "url": "/OkosCucc_kart/static/js/2.500dd1b8.chunk.js"
+  },
+  {
+    "revision": "b9941b26ce7f87a25d1a3bc0d5ae97bc",
+    "url": "/OkosCucc_kart/static/js/2.500dd1b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "843d4029bbd4d0b10570",
+    "url": "/OkosCucc_kart/static/js/main.c3fc5ce6.chunk.js"
+  },
+  {
+    "revision": "0ad924b32e176f5e3642",
+    "url": "/OkosCucc_kart/static/js/runtime-main.71f2553b.js"
+  }
+]);
